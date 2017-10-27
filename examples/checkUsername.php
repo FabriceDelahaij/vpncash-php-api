465e009635fda12d5b9680c6e4f5b937217350dd
Fabrice Delahaij <info@extreemhost.nl>
2017-10-27 18:15:33 +0200

    Version 1.0

--- examples/checkUsername.php
+++ examples/checkUsername.php
@@ -3,11 +3,11 @@
 require_once 'vendor/autoload.php';
 
 try {
-    $api = new VPNCash_Client();
-    $data = [
-        'vpn_username' => '',
-    ];
-    $api->checkUsername($data);
+	$api = new VPNCash_Client();
+	$data = [
+		'vpn_username' => ''
+	];
+	$api->checkUsername($data);
 } catch (Exception $e) {
-    echo 'API call failed: '.htmlspecialchars($e->getMessage());
+	echo 'API call failed: '.htmlspecialchars($e->getMessage());
 }
