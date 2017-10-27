cda6941744d84c7fb41db692f43bae64a95fe069
Fabrice Delahaij <info@extreemhost.nl>
2017-10-27 17:44:31 +0200

    Version 1.0

---
+++ examples/createAccount.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+try {
+	$api = new VPNCash_Client();
+	$data = [
+		'access_token' => 'ID-TOKEN',
+		'vpn_username' => 'test123',
+		'vpn_password' => '123546
+	];
+	$api->createAccount($data);
+} catch (Exception $e) {
+	echo 'API call failed: '.htmlspecialchars($e->getMessage());
+}
